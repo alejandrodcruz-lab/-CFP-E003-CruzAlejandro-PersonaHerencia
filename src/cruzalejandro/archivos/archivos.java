98769e38c4e6f8b07f18488ca8fb9abcfd622fdd
Alejandro Cruz <alejandrodavidcruz@gmail.com>
2020-09-29 00:04:29 -0300

    Subida a Git

---
+++ src/cruzalejandro/archivos/archivos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cruzalejandro.archivos;
+
+/*
+
+/*
+2- Crear una clase Archivo con los atributos que crean necesarios, la clase tie-
+-ne que tener dos métodos leer y escribir , donde leer recibe como argumento el 
+archivo que tiene que leer y muestra el contenido por consola , y escribir reci-
+-be como argumento el nombre del archivo y el contenido que tiene que almacenar 
+en dicho archivo.
+*/
+
+/**
+ *
+ * @author EntornoVM
+ */
+public class archivos {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
--- src/cruzalejandropersonaherencia/UsoPersonaHerencia.java
+++ src/cruzalejandropersonaherencia/UsoPersonaHerencia.java
@@ -16,41 +16,38 @@ public class UsoPersonaHerencia {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //*****Persona*******//
         Persona persona1 = new Persona();
-        
+
         persona1.mostrar();
-        
+
         persona1.setNombre("Abby");
         persona1.setApellido("Cruz");
         persona1.setDni(33000000);
-        
+
         persona1.mostrar();
-        
+
         //*****Alumno*******//
         Alumno alumno1 = new Alumno();
-        
+
         alumno1.setNombre("Chimuelo");
-        
+
         alumno1.setNotas(9);
         alumno1.setNotas(7);
         alumno1.setNotas(9);
-        
+
         System.out.println(alumno1.getPromedio());
-        
+
         alumno1.mostrar();
-        
+
         //*****Docente*******//
-        
-        Docente docente1 =new Docente();
-        
+        Docente docente1 = new Docente();
+
         docente1.setNombre("Morena");
         docente1.setLegajo(1);
         docente1.mostrar();
-        
-       
-        
+
     }
-    
+
 }
