cbec407bedfda3831d8c8cded8025f4115183e7f
Alejandro Cruz <alejandrodavidcruz@gmail.com>
2020-09-29 00:19:01 -0300

    Subida a Git

--- src/cruzalejandro/archivos/archivos.java
+++ src/cruzalejandro/archivos/archivos.java
@@ -5,6 +5,11 @@
  */
 package cruzalejandro.archivos;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /*
 
 /*
@@ -13,17 +18,33 @@ package cruzalejandro.archivos;
 archivo que tiene que leer y muestra el contenido por consola , y escribir reci-
 -be como argumento el nombre del archivo y el contenido que tiene que almacenar 
 en dicho archivo.
-*/
-
+ */
 /**
  *
  * @author EntornoVM
  */
 public class archivos {
-    
+
     public static void main(String[] args) {
-        
+
+        System.out.println("Iniciando escritura...");
+        File f = new File("numeros.txt");
+        try {
+            FileWriter fw = new FileWriter(f);
+            PrintWriter pw = new PrintWriter(fw);
+
+            for (int i = 0; i < 20; i++) {
+
+                pw.println(i);
+                System.out.println(i);
+            }
+
+            fw.close();
+            System.out.println("Escritura finalizada");
+            
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
     }
-    
-    
+
 }
