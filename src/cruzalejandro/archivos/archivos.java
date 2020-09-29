432327b8eb8ebb5e1b35b61f75fadadc8967155f
Alejandro Cruz <alejandrodavidcruz@gmail.com>
2020-09-29 01:02:32 -0300

    Se agrega Clase Archivos

--- src/cruzalejandro/archivos/archivos.java
+++ src/cruzalejandro/archivos/archivos.java
@@ -5,7 +5,9 @@
  */
 package cruzalejandro.archivos;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -26,26 +28,51 @@ en dicho archivo.
 public class archivos {
 
     public static void main(String[] args) {
-
-        System.out.println("Iniciando escritura...");
+        
+        escribirArchivo("hola.txt");
+        leerArchivo("hola.txt");
+        leerArchivo("chau");
+    }
+ 
+    public static void escribirArchivo(String archivo) {
 
         try {
-            File f = new File("numeros.txt");
+            File f = new File(archivo);
             FileWriter fw = new FileWriter(f);
             PrintWriter pw = new PrintWriter(fw);
 
             for (int i = 0; i < 20; i++) {
 
                 pw.println(i);
-                System.out.println(i);
+                System.out.println("Escribiendo :"+i);
             }
 
             fw.close();
             System.out.println("Escritura finalizada");
 
         } catch (IOException e) {
-            System.out.println("Error");
+            System.out.println("Error al crear el archivo");
         }
     }
 
-}
+    public static void leerArchivo(String archivo) {
+
+        try {
+            File f = new File(archivo);
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String linea=br.readLine();
+
+            while (linea!= null) {
+
+                linea = br.readLine();
+                System.out.println("Leyendo :"+linea);
+            }
+            fr.close();
+            System.out.println("Lectura finalizada");
+        } catch (IOException e) {
+            System.out.println("Error al leer un archivo");
+        }
+
+    }
+};
